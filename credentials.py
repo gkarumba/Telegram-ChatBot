d3be919ca016f62f46adac3022f1bd6319acb1c3
gkarumba <gachegua@gmail.com>
2019-10-03 13:21:33 +0300

    make it better

---
+++ credentials.py
@@ -0,0 +1,17 @@
+# ===============================================================
+# Author: Rodolfo Ferro Pérez
+# Email: ferro@cimat.mx
+# Twitter: @FerroRodolfo
+#
+# ABOUT COPYING OR USING PARTIAL INFORMATION:
+# This script was originally created by Rodolfo Ferro. Any
+# explicit usage of this script or its contents is granted
+# according to the license provided and its conditions.
+# ===============================================================
+
+
+# Telegram credentials:
+telegram_token = '852762164:AAHwLOf0LH0DwVpE2SqLwtQDi_mLlDcgkMM'
+
+# Google Maps credentials:
+maps_token = 'AIzaSyCeFEqaXj_7P1xeCcV3zC2h1ldJ9GEj4GA'
