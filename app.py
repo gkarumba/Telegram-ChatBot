6fea0a24ea4df9b92f7040af7ad40bc4a91fbd5e
gkarumba <gachegua@gmail.com>
2019-10-03 14:26:48 +0300

    make it better4

--- app.py
+++ app.py
@@ -264,11 +264,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start DisAtBot:
-    updater.start_polling()
+    # updater.start_polling()
 
     # Run the bot until the user presses Ctrl-C or the process
     # receives SIGINT, SIGTERM or SIGABRT:
-    updater.idle()
+    # updater.idle()
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
